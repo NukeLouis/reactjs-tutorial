06e08249bb240ca089d6fccc31492ea6fb892dfe
Joris Boschmans <jorisboschmans@gmail.com>
2022-05-01 12:49:53 +0200

    Showing the past moves

--- src/index.js
+++ src/index.js
@@ -66,6 +66,17 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((ignored, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
         if (winner) status = 'Winner: ' + winner;
         else        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
@@ -80,7 +91,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
